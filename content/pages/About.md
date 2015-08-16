7f0b45d9884330ffe4d3a0955c41748a1421cdca
Antonino Ingargiola <tritemio@gmail.com>
2015-08-16 11:38:20 -0700

    Start blog and add some content

---
+++ content/pages/About.md
@@ -0,0 +1,6 @@
+Title: About
+Status: published
+
+A blog about single-molecules and scientific computing, mostly in python.
+
+See [First Post]({filename}/2015-08/first-post.md) for a more detailed overview.
---
+++ pelicanconf.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Antonino Ingargiola'
+SITENAME = u'Single-Molecules Bits'
+SITESUBTITLE = u'Of scientific computing and single molecules.'
+SITEDESCRIPTION = 'A blog about single-molecules and scientific computing.'
+SITEURL = ''#https://tritemio.github.io/smbits/'
+TWITTER_USERNAME = 'tritemio_sc'
+DISQUS_SITENAME = 'smbits'
+GOOGLE_ANALYTICS = ''
+
+PATH = 'content'
+DEFAULT_LANG = u'en'
+TIMEZONE = 'America/Los_Angeles'
+SLUGIFY_SOURCE = 'basename'
+TYPOGRIFY = True
+SUMMARY_MAX_LENGTH = 50
+DISPLAY_PAGES_ON_MENU = True
+
+DIRECT_TEMPLATES = ('index', 'archives')
+DEFAULT_LANG = 'en'
+#DATE_FORMATS = { 'en': '%B %d, %Y', }
+STATIC_PATHS = ['images', 'favicon.ico']
+ARTICLE_EXCLUDES = ['.ipynb_checkpoints', '2015-09/.ipynb_checkpoints']
+
+ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+DEFAULT_METADATA = {
+    'status': 'draft',
+}
+
+# Blogroll
+LINKS = (('Biophysical Society', 'http://www.biophysics.org/'),
+         ('Planet Scipy', 'http://planet.scipy.org/'),
+)
+
+# Social widget
+SOCIAL = (('Twitter', 'http://twitter.com/tritemio_sc'),
+          ('GitHub', 'http://github.com/tritemio'),
+          ('ResearchGate', 'https://www.researchgate.net/profile/Antonino_Ingargiola'),
+          ('Academia.edu', 'https://ucla.academia.edu/AntoninoIngargiola'),
+)
+
+DEFAULT_PAGINATION = 10
+
+THEME = 'notmyidea'
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+NOTEBOOK_DIR = 'notebooks'
+MARKUP = ['md']
+
+PLUGIN_PATHS = ['./pelican-plugins']
+PLUGINS = ['render_math', 'ipynb',
+           'liquid_tags.img', 'liquid_tags.video',
+           'liquid_tags.youtube', 'liquid_tags.vimeo',
+           'liquid_tags.include_code', 'liquid_tags.notebook',
+           ]
+
+#EXTRA_HEADER = open('_nb_header.html', 'rb').read().decode('utf-8')
